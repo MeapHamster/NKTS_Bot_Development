6d361300f0dab9660b91c0f55ca970a1a2c9f66b
Meap <thedcat101@gmail.com>
2021-12-10 14:08:33 -0600

    Fixed error sending messages in View_Data

--- Modules/View_Data.py
+++ Modules/View_Data.py
@@ -13,23 +13,31 @@ async def cmd_View_Data_Invoked(Variables):
     Get_User_Data_Cards = Variables['Get_User_Data_Cards']
     Trello_Data         = Variables['Trello_Data']
     # Variables #
-    DM_Channel = await message.author.create_dm()
     message_txt = message.content
     if Check_Command(message, MAIN_VARIABLES['PREFIX'], Channel_Whitelist, Command_Whitelists, 'View Data') == True:
+        Channel = await message.author.create_dm()
         User_Data_Cards = Get_User_Data_Cards(Trello_Data)
         Found_User_Data_Card = False
         for Card in User_Data_Cards:
             if Card['desc'].split('Discord_User_ID:')[1].split('\n')[0] == str(message.author.id):
                 Found_User_Data_Card = True
+                Send_Discord_User_ID = str(message.author.id)
+                Send_Roblox_Username = 'Data not found'
+                Send_Email_Address = 'Data not found'
+                if Card['desc'].split('Roblox_Username:')[1].split('\n')[0] != '':
+                    Send_Roblox_Username = Card['desc'].split('Roblox_Username:')[1].split('\n')[0]
+                if Card['desc'].split('Email_Address:')[1].split('\n')[0] != '':
+                    Send_Email_Address = Card['desc'].split('Email_Address:')[1].split('\n')[0]
                 embed = discord.Embed(
                     title = 'View User Data',
+                    description = '',
                     colour = discord.Colour.gold()
                 )
                 embed.set_footer(text = '• NKTS Secure Data')
-                embed.add_field(name = 'Discord User ID', value = Card['desc'].split('Discord_User_ID:')[1].split('\n')[0], inline = False)
-                embed.add_field(name = 'Roblox Username', value = Card['desc'].split('Roblox_Username:')[1].split('\n')[0], inline = False)
-                embed.add_field(name = 'Email Address', value = Card['desc'].split('Email_Address:')[1].split('\n')[0], inline = False)
-                await DM_Channel.send(embed = embed)
+                embed.add_field(name = 'Discord User ID', value = Send_Discord_User_ID, inline = False)
+                embed.add_field(name = 'Roblox Username', value = Send_Roblox_Username, inline = False)
+                embed.add_field(name = 'Email Address', value = Send_Email_Address, inline = False)
+                await Channel.send(embed = embed)
         if Found_User_Data_Card == False:
             embed = discord.Embed(
                 title = 'You blew up the internet.',
@@ -37,4 +45,4 @@ async def cmd_View_Data_Invoked(Variables):
                 colour = discord.Colour.gold()
             )
             embed.set_footer(text = '• NKTS')
-            await DM_Channel.send(embed = embed)
+            await Channel.send(embed = embed)
